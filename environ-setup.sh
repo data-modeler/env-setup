998181a6837bffd7b6d5b01a2c3857a402dd6f9d
Dale Gilliam <26942296+data-modeler@users.noreply.github.com>
2018-09-23 17:39:43 -0400

    Install docker-compose

--- environ-setup.sh
+++ environ-setup.sh
@@ -43,4 +43,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # install Docker
-sudo apt install -y docker-ce
+sudo apt install -y docker-ce docker-compose
